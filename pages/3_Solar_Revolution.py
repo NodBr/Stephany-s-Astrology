7934f0c0971901dfdc0947b92e93f13174f1999f
NodBr <nathanoctavio@gmail.com>
2024-10-06 18:01:35 -0300

    Solar Revolution: Options for Map Style

--- pages/3_Solar_Revolution.py
+++ pages/3_Solar_Revolution.py
@@ -16,7 +16,7 @@ birth_data()  # Collect birth data input
 # Check if birth date is provided
 if st.session_state.bday_date is not None:
     st.header('Solar Revolution Criteria')
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     
     # Select Solar Revolution Year
     st.session_state.sr_year = col1.number_input(
@@ -32,19 +32,6 @@ if st.session_state.bday_date is not None:
     
     # Select view (planet or Ascendant)
     st.session_state.sr_view = col2.selectbox(label='View', options=planet_names)
-    
-    # Set filter criteria based on the view selected
-    if st.session_state.sr_view == 'Ascendant':
-        sign_options = ['All'] + [sign['name'] for sign in st.session_state.signs.values()]
-        st.session_state.sr_filter = col3.selectbox(
-            label='Sign Filter', 
-            options=sign_options
-        )
-    else:
-        st.session_state.sr_filter = col3.selectbox(
-            label='House Filter', 
-            options=['All houses'] + [house['name'] for house in st.session_state.houses.values()]
-        )
 
 # Run button to execute calculations
 if st.button(label='Run'):
@@ -68,7 +55,7 @@ if st.button(label='Run'):
                 'latitude': lat,
                 'longitude': lon,
                 'number': calculate_sign(swe.houses(solar_cross_julian_day, lat, lon)[0][0]),
-                'rgb_color': tuple(st.session_state.signs[calculate_sign(swe.houses(solar_cross_julian_day, lat, lon)[0][0])]['rgb_color']),  # Convert list to tuple
+                'rgb_color': tuple(st.session_state.signs[calculate_sign(swe.houses(solar_cross_julian_day, lat, lon)[0][0])]['rgb_color']),
                 'caption': st.session_state.signs[calculate_sign(swe.houses(solar_cross_julian_day, lat, lon)[0][0])]['name']
             }
             for lat in range(-66, 67)
@@ -94,47 +81,75 @@ if st.button(label='Run'):
 
     results_df = pd.DataFrame(results)
 
-    # Apply the sign filter if the view is 'Ascendant'
-    if st.session_state.sr_view == 'Ascendant' and st.session_state.sr_filter != 'All':
-        filtered_df = results_df[results_df['caption'] == st.session_state.sr_filter]
-    elif st.session_state.sr_view != 'Ascendant' and st.session_state.sr_filter != 'All houses':
-        filtered_df = results_df[results_df['caption'] == st.session_state.sr_filter]
-    else:
-        filtered_df = results_df  # No filtering or "All" selected
-
-    # Sort the DataFrame by 'number' to maintain natural order of signs
-    results_df_sorted = filtered_df[['number', 'caption', 'rgb_color']].drop_duplicates().sort_values(by='number')
-
-    # Create a Pydeck layer for the map using the filtered DataFrame
-    layer = pdk.Layer(
-        'ScatterplotLayer',
-        data=filtered_df,
-        get_position='[longitude, latitude]',
-        get_fill_color='[rgb_color[0], rgb_color[1], rgb_color[2], 120]',  # RGBA with transparency
-        get_radius=30000,
-        pickable=True
-    )
-
-    # Set up the map view
-    view_state = pdk.ViewState(
-        latitude=0,
-        longitude=0,
-        zoom=1
-    )
-
     # Render the map with Pydeck
-    st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state))
+    @st.fragment
+    def map_render():        
+        # Set filter criteria based on the view selected
+        col1, col2 = st.columns(2)
+        if st.session_state.sr_view == 'Ascendant':
+            sign_options = ['All'] + [sign['name'] for sign in st.session_state.signs.values()]
+            st.session_state.sr_filter = col1.selectbox(
+                label='Sign Filter', 
+                options=sign_options
+            )
+        else:
+            st.session_state.sr_filter = col1.selectbox(
+                label='House Filter', 
+                options=['All houses'] + [house['name'] for house in st.session_state.houses.values()]
+            )
 
-    # Create a legend using the sorted DataFrame
-    unique_signs = results_df_sorted.to_dict(orient='records')
-    cols = st.columns(4)
+        # Apply the sign filter if the view is 'Ascendant'
+        if st.session_state.sr_view == 'Ascendant' and st.session_state.sr_filter != 'All':
+            filtered_df = results_df[results_df['caption'] == st.session_state.sr_filter]
+        elif st.session_state.sr_view != 'Ascendant' and st.session_state.sr_filter != 'All houses':
+            filtered_df = results_df[results_df['caption'] == st.session_state.sr_filter]
+        else:
+            filtered_df = results_df  # No filtering or "All" selected
 
-    # Distribute 12 items across 4 columns
-    for idx, sign_data in enumerate(unique_signs):
-        color = sign_data['rgb_color']
-        caption = sign_data['caption']
-        col_idx = idx % 4
-        cols[col_idx].markdown(
-            f'<span style="display: inline-block; width: 16px; height: 16px; background-color: rgb({color[0]}, {color[1]}, {color[2]}); margin-right: 8px;"></span> {caption}', 
-            unsafe_allow_html=True
+        # Sort the DataFrame by 'number' to maintain natural order of signs
+        results_df_sorted = filtered_df[['number', 'caption', 'rgb_color']].drop_duplicates().sort_values(by='number')
+
+        # Initialize the layer variable here to avoid UnboundLocalError
+        layer = None
+
+        # Create a Pydeck layer for the map using the filtered DataFrame
+        if not filtered_df.empty:  # Ensure that there is data to plot
+            layer = pdk.Layer(
+                'ScatterplotLayer',
+                data=filtered_df,
+                get_position='[longitude, latitude]',
+                get_fill_color='[rgb_color[0], rgb_color[1], rgb_color[2], 120]',  # RGBA with transparency
+                get_radius=30000,
+                pickable=True
+            )
+
+        # Set up the map view
+        view_state = pdk.ViewState(
+            latitude=0,
+            longitude=0,
+            zoom=1
         )
+
+        # Select the map style
+        map_style = col2.selectbox(label='Map Style', options=['Light', 'Dark', 'Road'])
+        
+        # Check if the layer is defined before attempting to render the map
+        if layer:
+            st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state, map_style=map_style.lower()))
+        else:
+            st.write("No data available to plot.")
+
+        # Create a legend using the sorted DataFrame
+        unique_signs = results_df_sorted.to_dict(orient='records')
+        cols = st.columns(4)
+
+        # Distribute 12 items across 4 columns
+        for idx, sign_data in enumerate(unique_signs):
+            color = sign_data['rgb_color']
+            caption = sign_data['caption']
+            col_idx = idx % 4
+            cols[col_idx].markdown(
+                f'<span style="display: inline-block; width: 16px; height: 16px; background-color: rgb({color[0]}, {color[1]}, {color[2]}); margin-right: 8px;"></span> {caption}', 
+                unsafe_allow_html=True
+            )
+    map_render()
